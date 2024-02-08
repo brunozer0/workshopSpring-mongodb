0e5eb287107f8eef02895ef48ce0a778315d6f69
Bruno Costa <magalhaes.brunno1@gmail.com>
2024-02-08 13:37:57 -0300

    referencia post users

--- src/main/java/com/bruno/workshopmongo/config/Instantiation.java
+++ src/main/java/com/bruno/workshopmongo/config/Instantiation.java
@@ -30,19 +30,21 @@ public class Instantiation implements CommandLineRunner {
 
 		userRepository.deleteAll();
 		postRepository.deleteAll();
-		
+
 		User maria = new User(null, "Maria Brown", "maria@gmail.com");
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
-		
 
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
-		
+
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu espanha",
 				"vou viajar para a espanha no proximo mes", new AuthorDTO(maria));
-		Post post2 = new Post(null, sdf.parse("17/05/2018"), "Churrasco hoje", "hoje tem churrasco!!!!", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("17/05/2018"), "Churrasco hoje", "hoje tem churrasco!!!!",
+				new AuthorDTO(maria));
 		postRepository.saveAll(Arrays.asList(post1, post2));
+
+		maria.getPosts().addAll(Arrays.asList(post1, post2));
+		userRepository.save(maria);
 	}
 
 }
